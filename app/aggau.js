b28ac627e6574d5be57454813a405368fcbc98d1
Dmitry Sidorov <sidr@sidora.net>
2016-02-05 13:42:59 +0800

    redis store

--- app/aggau.js
+++ app/aggau.js
@@ -9,15 +9,25 @@ var express = require("express");
 var app = module.exports = express();
 var log = require("log")(module);
 
-var config = require("conf");
+var conf = require("conf");
 var json = require("express-json");
-var cookie = require('cookie');
+//var cookie = require('cookie');
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
 //var bodyParser = require("body-parser");
-//var RedisStore = require("connect-redis")(session);
+
+
+var Promise = require('bluebird');
+var RedisStore = require("connect-redis")(session);
+
+var Redis = require("redis");
+Promise.promisifyAll(Redis.RedisClient.prototype);
+Promise.promisifyAll(Redis.Multi.prototype);
+var redis = Redis.createClient(conf("redis:port"), conf("redis:host"));
+
+
 var http = require('http').Server(app);
-var io = app.io = require("socket.io")(http);
+//var io = app.io = require("socket.io")(http);
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "jade");
@@ -27,22 +37,72 @@ app.set("view engine", "jade");
 app.use(cookieParser());
 app.use(json());
 
-//var sessionStore = new RedisStore(config.get("redis-store"));
-//app.use(session({
-//    key: config.get("session:key"),
-//    store: sessionStore,
-//    secret: config.get("session:secret"),
-//    cookie: config.get("session:cookie"),
-//    rolling: true,
-//    unset: "destroy",
-//    maxAge: null
-//}));
-//app.use(require("middlewares/helpers").flashMessages);
-//app.use(require("middlewares/helpers").env_vars);
+var sessionStore = new RedisStore(conf("redis"));
+app.use(session({
+    key: conf("session:key"),
+    store: sessionStore,
+    secret: conf("session:secret"),
+    cookie: conf("session:cookie"),
+    rolling: true,
+    unset: "destroy",
+    maxAge: null
+}));
+
+app.use(function(req, res, next){
+    req.redis = res.redis = redis;
+    next();
+});
 
 
 // подключаем и инициализируем роуты
 require("./routes")(app);
+
+
+if (!module.parent) { // it is for Zombie
+    http.listen(conf("port"), function () {
+        log.info("Aggau is listening on port ", conf("port"), "(mode=" + conf("NODE_ENV") + ")");
+
+        var testRedis = function() {
+            var now = new Date();
+            var time = now.getTime();
+
+            redis.zadd(["log:server", time, "start"], function(err, response){
+                if (err) throw err;
+                log.info("log message added");
+            });
+
+            var moment = require("moment");
+            //log.info(moment(time, 'x').format("DD MM YYYY HH:mm"));
+
+
+            redis.set("server:time_start", time);
+            redis.getAsync("server:time_start")
+                .then(function(value) {
+                    if (1*value !== time) {
+                        //throw new Error("Wrong redis set/get routine");
+                        log.error("Wrong redis set/get routine: " + value + "!=" + time);
+                        process.exit();
+                    }
+                });
+        }
+
+        if (conf("redis:pass")) {
+            redis.auth(conf("redis:pass"), function(err){
+                if (err) {
+                    // TODO write redis log
+                    log.error("Redis auth failed: " + err.message);
+                    process.exit();
+                } else {
+                    testRedis();
+                }
+            });
+        } else {
+            testRedis();
+        }
+
+    });
+}
+
 
 //io.use(function ioSession(socket, next) {
 //    //log.error("io.use iSession");
@@ -74,10 +134,3 @@ require("./routes")(app);
 //        next(new Error('Ошибка сервера'));
 //    }
 //});
-
-
-if (!module.parent) { // it is for Zombie
-    http.listen(config("port"), function () {
-        log.info("Aggau is listening on port ", config("port"), "(mode=" + config("NODE_ENV") + ")");
-    });
-}
--- app/routes/index.js
+++ app/routes/index.js
@@ -6,14 +6,24 @@
 
 module.exports = function(app) {
     var user = require("./user");
+    var moment = require("moment");
+    moment.locale("ru-ru", {});
+
+    var log = require("log")(module);
 
     app.get("/", user.isGuest, function(req, res){
         res.status(200).send("OK");
     });
 
     app.get("/admin", user.isAdmin, function(req, res){
-
         res.status(200).send("OK");
+    });
+
+    app.get("/up", user.isGuest, function(req, res){
+        req.redis.getAsync("server:time_start")
+            .then(function(time){
+                res.status(200).send(moment(time, 'x').toNow(true));
+            });
     });
 
 }
